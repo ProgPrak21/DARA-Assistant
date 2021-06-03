d912a2644c5f6c179d4847c657d54efca8f77aff
flow <git@lowspace.de>
2021-06-03 14:34:54 +0200

    Working facebook request and download scripts

--- src/background.ts
+++ src/background.ts
@@ -1,65 +1,55 @@
+async function injectFunction(tabId: number, functionName: Function) {
+  const funString = functionName.toString();
+  const funBody = funString.slice(funString.indexOf("{") + 1, funString.lastIndexOf("}"));
+  await chrome.tabs.executeScript(tabId,{
+    code: funBody,
+  });
+}
+
+import {
+  facebookRequest,
+  facebookCheck,
+  facebookDownload,
+} from "./scripts/facebook";
+
 try {
-  
+
   // This file is ran as a background script
   console.log("Hello from background script!");
-  
-  
-  function executeScript(tab, functionName) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: functionName,
-  });
-  }
 
-  import {
-    facebookRequest,
-    facebookCheck,
-    facebookDownload,
-  } from "./scripts/facebook";
+  let services = ["facebook"];
 
+  // Listener for the messages from extension ()
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Message received in background.js!", request);
 
-    let services = ["facebook"];
-
-    // Listener for the messages from extension ()
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      console.log("Message received in background.js!", request);
-
-      // Listen for tabs update
-      chrome.tabs.onUpdated.addListener(function onUpdated(tabId, changeInfo, tab) {
-        //check if the tab has been loaded
-        if (tab.status === "complete") {
-          console.log("The new tab has been loaded");
+    // Listen for tabs update
+    chrome.tabs.onUpdated.addListener(async function onUpdated(tabId, changeInfo, tab) {
+      //check if the tab has been loaded
+      if (tab.status === "complete") {
+        console.log("The new tab has been loaded");
 
         // check for which service, type of request
         const { host } = new URL(tab.url ?? "");
         const { type } = request;
         console.log(host);
-        
+
         if (host === "www.facebook.com") {
-          if (type === "request") facebookRequest();
-      
+          if (type === "request") {
+            console.log("Injecting request script");
+            await injectFunction(tabId, facebookRequest);
+          }
+
           if (type === "check") {
             console.log("Sending Message via runtime");
-            let result = await facebookCheck();
-            console.log("result ===> ", result);
-            chrome.runtime.sendMessage(result);
+            //let result = await injectFunction(tab, facebookCheck);
+            await injectFunction(tabId, facebookCheck);
+            //console.log("result ===> ", result);
+            //chrome.runtime.sendMessage(result);
           }
-      
-          if (type === "download") await facebookDownload();
+
+          if (type === "download") await injectFunction(tabId, facebookDownload);
         }
-        
-        //send request to content script
-        /*
-        console.log("Sending Request to content script");
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          tabs.forEach((tab) => {
-            if (tab.id === tabId) {
-              console.log("Tab Status ===> ", tab.status);
-              chrome.tabs.sendMessage(tab.id, { host, type });
-            }
-          });
-        });
-        */
         chrome.tabs.onUpdated.removeListener(onUpdated);
       }
     });
