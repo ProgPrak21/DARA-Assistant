2d1c0ae668e14d4f6424cf4963161617cb9d5dc9
flow <git@lowspace.de>
2021-06-04 14:04:37 +0200

    Add empty setTimeout() functions

--- src/connectors/facebook.ts
+++ src/connectors/facebook.ts
@@ -1,91 +1,96 @@
 export const request = () => {
-  console.log("Executing Facebook Request!");
-  //Select all iframes
-  Array.from(document.querySelectorAll("iframe")).forEach((iframe) => {
-    if (iframe.contentWindow) {
-      //Select Format
-      Array.from(iframe.contentWindow.document.body.querySelectorAll("label"))
-        ?.find((e) => e?.textContent?.startsWith("Format"))
-        ?.querySelector("a")
-        ?.click();
+  setTimeout(() => {
+    console.log("Executing Facebook Request!");
+    //Select all iframes
+    Array.from(document.querySelectorAll("iframe")).forEach((iframe) => {
+      if (iframe.contentWindow) {
+        //Select Format
+        Array.from(iframe.contentWindow.document.body.querySelectorAll("label"))
+          ?.find((e) => e?.textContent?.startsWith("Format"))
+          ?.querySelector("a")
+          ?.click();
 
-      //Find The json button in the menu
-      let arr = Array.from(
-        iframe.contentWindow.document.body.querySelectorAll(
-          'a[role="menuitemcheckbox"]'
-        )
-      );
+        //Find The json button in the menu
+        let arr = Array.from(
+          iframe.contentWindow.document.body.querySelectorAll(
+            'a[role="menuitemcheckbox"]'
+          )
+        );
 
-      let json: HTMLElement = arr?.find(
-        (e) => e.textContent === "JSON"
-      ) as HTMLElement;
+        let json: HTMLElement = arr?.find(
+          (e) => e.textContent === "JSON"
+        ) as HTMLElement;
 
-      json.click();
+        json.click();
 
-      //Click on Create File button
-      iframe.contentWindow &&
-        Array.from(
-          iframe.contentWindow.document.body.querySelectorAll("button")
-        )
-          ?.find((el) => el.textContent === "Create File")
-          ?.click?.();
-    }
+        //Click on Create File button
+        iframe.contentWindow &&
+          Array.from(
+            iframe.contentWindow.document.body.querySelectorAll("button")
+          )
+            ?.find((el) => el.textContent === "Create File")
+            ?.click?.();
+      }
+    });
   });
 };
 
 export const check = () => {
-  console.log("Executing Facebook Check!");
-  let result;
-  let pending = Array.from(document.querySelectorAll("iframe")).map(
-    (iframe) =>
-      iframe.contentWindow &&
-      Array.from(
-        iframe.contentWindow.document.body.querySelectorAll(
-          'div[role="heading"]'
+  setTimeout(() => {
+    console.log("Executing Facebook Check!");
+    let result;
+    let pending = Array.from(document.querySelectorAll("iframe")).map(
+      (iframe) =>
+        iframe.contentWindow &&
+        Array.from(
+          iframe.contentWindow.document.body.querySelectorAll(
+            'div[role="heading"]'
+          )
+        ).find((e) =>
+          e.textContent?.startsWith(
+            "A copy of your information is being created."
+          )
         )
-      ).find((e) =>
-        e.textContent?.startsWith(
-          "A copy of your information is being created."
-        )
-      )
-  );
+    );
 
-  let ready = Array.from(document.querySelectorAll("iframe")).map(
-    (iframe) =>
-      iframe.contentWindow &&
-      Array.from(
-        iframe.contentWindow.document.body.querySelectorAll(
-          'div[data-hover="tooltip"]'
-        )
-      ).find((e) => e.textContent?.startsWith("Download"))
-  );
+    let ready = Array.from(document.querySelectorAll("iframe")).map(
+      (iframe) =>
+        iframe.contentWindow &&
+        Array.from(
+          iframe.contentWindow.document.body.querySelectorAll(
+            'div[data-hover="tooltip"]'
+          )
+        ).find((e) => e.textContent?.startsWith("Download"))
+    );
 
-  result = "none";
-  if (!pending.includes(undefined) && pending.length === 1) {
-    console.log("Request is pending!");
-    result = "pending";
-  }
-  if (!ready.includes(undefined) && ready.length === 1) {
-    console.log("Request is Ready");
-    result = "ready";
-  }
-  // Send message to background that our request is ready
-  chrome.runtime.sendMessage({ requestState: result });
+    result = "none";
+    if (!pending.includes(undefined) && pending.length === 1) {
+      result = "pending";
+    }
+    if (!ready.includes(undefined) && ready.length === 1) {
+      result = "ready";
+    }
+    console.log(`Request is ${result}!`);
+    // Send message to background that our request is ready
+    chrome.runtime.sendMessage({ requestState: result });
+  });
 };
 
 export const download = () => {
-  console.log("Executing Facebook Download!");
-  let downloadBtn: HTMLElement = Array.from(
-    document.querySelectorAll("iframe")
-  ).map(
-    (iframe) =>
-      iframe.contentWindow &&
-      Array.from(
-        iframe.contentWindow.document.body.querySelectorAll(
-          'div[data-hover="tooltip"]'
-        )
-      )?.find((e) => e?.textContent?.startsWith("Download"))
-  )[0] as HTMLElement;
+  setTimeout(() => {
+    console.log("Executing Facebook Download!");
+    let downloadBtn: HTMLElement = Array.from(
+      document.querySelectorAll("iframe")
+    ).map(
+      (iframe) =>
+        iframe.contentWindow &&
+        Array.from(
+          iframe.contentWindow.document.body.querySelectorAll(
+            'div[data-hover="tooltip"]'
+          )
+        )?.find((e) => e?.textContent?.startsWith("Download"))
+    )[0] as HTMLElement;
 
-  downloadBtn.click();
+    downloadBtn.click();
+  });
 };
