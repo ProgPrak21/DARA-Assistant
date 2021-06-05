4b4005d2b7416fd2efc8e65a6671bb97d336c347
majedidilbi <majed.idlby@gmail.com>
2021-06-05 11:03:46 +0200

    implement check asynchronously

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,21 @@
+//Inject function asynchronously
 function injectFunction(tabId: number, functionName: Function) {
-  const funString = functionName.toString();
-  const funBody = funString.slice(funString.indexOf("{") + 1, funString.lastIndexOf("}"));
+  const functionString = functionName.toString();
+  const functionBody = functionString.slice(
+    functionString.indexOf("{") + 1,
+    functionString.lastIndexOf("}")
+  );
   chrome.tabs.executeScript(tabId, {
-    code: funBody,
+    code: `(async () => {${functionBody}})()`,
   });
 }
 
 async function connectorExists(serviceName: string) {
-  const connector = await import(`./connectors/${serviceName}.ts`).catch(function () { return false });
+  const connector = await import(`./connectors/${serviceName}.ts`).catch(
+    function () {
+      return false;
+    }
+  );
   return connector;
 }
 
@@ -15,23 +23,31 @@ console.log("Hello from background script!");
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("Message received in background!", message);
-  if (['request', 'check', 'download'].includes(message.type)) {
+  if (["request", "check", "download"].includes(message.type)) {
     // Listen for tabs update
-    chrome.tabs.onUpdated.addListener(async function onUpdated(tabId, changeInfo, tab) {
+    chrome.tabs.onUpdated.addListener(async function onUpdated(
+      tabId,
+      changeInfo,
+      tab
+    ) {
       //check if the tab has been loaded
       console.log("Received onUpdated event.", tab, changeInfo);
-      if (tab.status === "complete" && changeInfo.status === "complete" && tabId === message.id) {
+      if (
+        tab.status === "complete" &&
+        changeInfo.status === "complete" &&
+        tabId === message.id
+      ) {
         console.log("Our tab has been loaded.", tab, changeInfo);
 
         const { hostname } = new URL(tab.url ?? "");
         const hostNameSplited = hostname.split(".");
         let part: string;
         let connector: any;
-        
+
         for (part of hostNameSplited) {
           connector = await connectorExists(part);
           if (connector !== false) {
-            console.log(`Found matching ${part} connector.`)
+            console.log(`Found matching ${part} connector.`);
             const { type } = message;
 
             console.log(`Injecting ${type} script`);
@@ -40,7 +56,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
           }
         }
         if (connector === false) {
-          console.log('Could not find connector for the requested domain.')
+          console.log("Could not find connector for the requested domain.");
         }
 
         chrome.tabs.onUpdated.removeListener(onUpdated);
--- src/connectors/facebook.ts
+++ src/connectors/facebook.ts
@@ -1,96 +1,107 @@
 export const request = () => {
-  setTimeout(() => {
-    console.log("Executing Facebook Request!");
-    //Select all iframes
-    Array.from(document.querySelectorAll("iframe")).forEach((iframe) => {
-      if (iframe.contentWindow) {
-        //Select Format
-        Array.from(iframe.contentWindow.document.body.querySelectorAll("label"))
-          ?.find((e) => e?.textContent?.startsWith("Format"))
-          ?.querySelector("a")
-          ?.click();
+  console.log("Executing Facebook Request!");
+  //Select all iframes
+  Array.from(document.querySelectorAll("iframe")).forEach((iframe) => {
+    if (iframe.contentWindow) {
+      //Select Format
+      Array.from(iframe.contentWindow.document.body.querySelectorAll("label"))
+        ?.find((e) => e?.textContent?.startsWith("Format"))
+        ?.querySelector("a")
+        ?.click();
 
-        //Find The json button in the menu
-        let arr = Array.from(
-          iframe.contentWindow.document.body.querySelectorAll(
-            'a[role="menuitemcheckbox"]'
-          )
-        );
+      //Find The json button in the menu
+      let arr = Array.from(
+        iframe.contentWindow.document.body.querySelectorAll(
+          'a[role="menuitemcheckbox"]'
+        )
+      );
 
-        let json: HTMLElement = arr?.find(
-          (e) => e.textContent === "JSON"
-        ) as HTMLElement;
+      let json: HTMLElement = arr?.find(
+        (e) => e.textContent === "JSON"
+      ) as HTMLElement;
 
-        json.click();
+      json.click();
 
-        //Click on Create File button
-        iframe.contentWindow &&
-          Array.from(
-            iframe.contentWindow.document.body.querySelectorAll("button")
-          )
-            ?.find((el) => el.textContent === "Create File")
-            ?.click?.();
-      }
-    });
+      //Click on Create File button
+      iframe.contentWindow &&
+        Array.from(
+          iframe.contentWindow.document.body.querySelectorAll("button")
+        )
+          ?.find((el) => el.textContent === "Create File")
+          ?.click?.();
+    }
   });
 };
 
-export const check = () => {
-  setTimeout(() => {
-    console.log("Executing Facebook Check!");
-    let result;
-    let pending = Array.from(document.querySelectorAll("iframe")).map(
-      (iframe) =>
-        iframe.contentWindow &&
-        Array.from(
-          iframe.contentWindow.document.body.querySelectorAll(
-            'div[role="heading"]'
-          )
-        ).find((e) =>
-          e.textContent?.startsWith(
-            "A copy of your information is being created."
-          )
+export const check = async () => {
+  console.log("Executing Facebook Check!");
+
+  const pause = (time: number) =>
+    new Promise((resolve) => setTimeout(resolve, time));
+
+  let allIframes = Array.from(document.querySelectorAll("iframe"));
+
+  //check if the iframes are loaded
+  if (!allIframes.length) {
+    for (let i = 0; i < 5; i++) {
+      console.log("check number ", i);
+      allIframes = Array.from(document.querySelectorAll("iframe"));
+      await pause(2000);
+      console.log(allIframes.length);
+      if (allIframes.length > 1) break;
+    }
+  }
+
+  let result;
+  let pending = Array.from(document.querySelectorAll("iframe")).map(
+    (iframe) =>
+      iframe.contentWindow &&
+      Array.from(
+        iframe.contentWindow.document.body.querySelectorAll(
+          'div[role="heading"]'
         )
-    );
+      ).find((e) =>
+        e.textContent?.startsWith(
+          "A copy of your information is being created."
+        )
+      )
+  );
 
-    let ready = Array.from(document.querySelectorAll("iframe")).map(
-      (iframe) =>
-        iframe.contentWindow &&
-        Array.from(
-          iframe.contentWindow.document.body.querySelectorAll(
-            'div[data-hover="tooltip"]'
-          )
-        ).find((e) => e.textContent?.startsWith("Download"))
-    );
+  let ready = Array.from(document.querySelectorAll("iframe")).map(
+    (iframe) =>
+      iframe.contentWindow &&
+      Array.from(
+        iframe.contentWindow.document.body.querySelectorAll(
+          'div[data-hover="tooltip"]'
+        )
+      ).find((e) => e.textContent?.startsWith("Download"))
+  );
 
-    result = "none";
-    if (!pending.includes(undefined) && pending.length === 1) {
-      result = "pending";
-    }
-    if (!ready.includes(undefined) && ready.length === 1) {
-      result = "ready";
-    }
-    console.log(`Request is ${result}!`);
-    // Send message to background that our request is ready
-    chrome.runtime.sendMessage({ requestState: result });
-  });
+  result = "none";
+  if (!pending.includes(undefined) && pending.length === 1) {
+    result = "pending";
+  }
+  if (!ready.includes(undefined) && ready.length === 1) {
+    result = "ready";
+  }
+  console.log(`Request is ${result}!`);
+  // Send message to background that our request is ready
+  chrome.runtime.sendMessage({ requestState: result });
 };
 
 export const download = () => {
-  setTimeout(() => {
-    console.log("Executing Facebook Download!");
-    let downloadBtn: HTMLElement = Array.from(
-      document.querySelectorAll("iframe")
-    ).map(
-      (iframe) =>
-        iframe.contentWindow &&
-        Array.from(
-          iframe.contentWindow.document.body.querySelectorAll(
-            'div[data-hover="tooltip"]'
-          )
-        )?.find((e) => e?.textContent?.startsWith("Download"))
-    )[0] as HTMLElement;
+  console.log("Executing Facebook Download!");
+  let downloadBtn: HTMLElement = Array.from(
+    document.querySelectorAll("iframe")
+  ).map(
+    (iframe) =>
+      iframe.contentWindow &&
+      Array.from(
+        iframe.contentWindow.document.body.querySelectorAll(
+          'div[data-hover="tooltip"]'
+        )
+      )?.find((e) => e?.textContent?.startsWith("Download"))
+  )[0] as HTMLElement;
 
-    downloadBtn.click();
-  });
+  downloadBtn.click();
 };
