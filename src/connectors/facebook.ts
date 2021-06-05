9a99a1876c6fa08311fe8bfefb4ae76dca6c01e5
flow <git@lowspace.de>
2021-06-05 15:25:54 +0200

    Add iframe validation to all methods

--- src/connectors/facebook.ts
+++ src/connectors/facebook.ts
@@ -1,8 +1,34 @@
-export const request = () => {
+export const request = async () => {
   console.log("Executing Facebook Request!");
-  //Select all iframes
-  Array.from(document.querySelectorAll("iframe")).forEach((iframe) => {
+
+  const pause = (time: number) =>
+    new Promise(
+      (resolve) => setTimeout(resolve, time)
+    );
+
+  //Get all the iframes, and filter out the ads
+  const getIframes = () => {
+    return Array.from(document.querySelectorAll("iframe")).filter(
+      (e) => !e.src.includes("common/referer_frame.php")
+    );
+  };
+
+  let allIframes = getIframes();
+  console.log('Got Iframes:', allIframes);
+
+  //check if the iframes are loaded, and retry for 5 times if not
+  if (!allIframes.length) {
+    for (let i = 0; i < 5; i++) {
+      console.log('Iframes still not loaded, trying again.');
+      allIframes = getIframes();
+      await pause(2000);
+      if (allIframes.length) break;
+    }
+  }
+
+  allIframes.forEach((iframe) => {
     if (iframe.contentWindow) {
+
       //Select Format
       Array.from(iframe.contentWindow.document.body.querySelectorAll("label"))
         ?.find((e) => e?.textContent?.startsWith("Format"))
@@ -72,6 +98,7 @@ export const check = async () => {
         )
       )
   );
+  console.log('Pending:', pending);
 
   let ready = allIframes.map(
     (iframe) =>
@@ -82,6 +109,7 @@ export const check = async () => {
         )
       ).find((e) => e.textContent?.startsWith("Download"))
   );
+  console.log('Ready:', ready);
 
   result = "none";
   if (!pending.includes(undefined) && pending.length === 1) {
@@ -91,15 +119,40 @@ export const check = async () => {
     result = "ready";
   }
   console.log(`Request is ${result}!`);
-  // Send message to background that our request is ready
+
+  // Send message to popup that our request is ready
   chrome.runtime.sendMessage({ requestState: result });
 };
 
-export const download = () => {
+export const download = async () => {
   console.log("Executing Facebook Download!");
-  let downloadBtn: HTMLElement = Array.from(
-    document.querySelectorAll("iframe")
-  ).map(
+
+  const pause = (time: number) =>
+    new Promise(
+      (resolve) => setTimeout(resolve, time)
+    );
+
+  //Get all the iframes, and filter out the ads
+  const getIframes = () => {
+    return Array.from(document.querySelectorAll("iframe")).filter(
+      (e) => !e.src.includes("common/referer_frame.php")
+    );
+  };
+
+  let allIframes = getIframes();
+  console.log('Got Iframes:', allIframes);
+
+  //check if the iframes are loaded, and retry for 5 times if not
+  if (!allIframes.length) {
+    for (let i = 0; i < 5; i++) {
+      console.log('Iframes still not loaded, trying again.');
+      allIframes = getIframes();
+      await pause(2000);
+      if (allIframes.length) break;
+    }
+  }
+
+  let downloadBtn: HTMLElement = allIframes.map(
     (iframe) =>
       iframe.contentWindow &&
       Array.from(
