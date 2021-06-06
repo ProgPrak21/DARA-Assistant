51314649e45345a4ed34e11d3bfb538430d18da7
flow <git@lowspace.de>
2021-06-06 22:27:52 +0200

    Add request to amazon.ts

---
+++ src/connectors/amazon.ts
@@ -0,0 +1,13 @@
+export const request = async() => {
+    document.querySelector<HTMLElement>("span.a-button-text,a-declarative[id='a-autoid-0-announce']")?.click();
+    document.getElementById('aedu-dsar-data-categories-dropdown_15')?.click();
+    document.querySelector<HTMLElement>("input.a-button-input[aria-labelledby='aedu-dsar-create-btn-announce']")?.click();
+};
+
+export const check = async () => {
+
+};
+
+export const download = async () => {
+
+};
