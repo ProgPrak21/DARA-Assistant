651c25f61d7e042e0b8a9c3552c1631071dd6eab
flow <git@lowspace.de>
2021-06-16 16:46:39 +0200

    Fix link targets

--- src/components/DefaultInfo.tsx
+++ src/components/DefaultInfo.tsx
@@ -12,11 +12,11 @@ export const DefaultInfo = ({ hostname }:props) => {
             <Grid item xs={12}>
                 <p>
                 We currently don't support automated requests on {hostname}.
-                You can send a email based request via <a className="App-link" href="https://www.mydatadoneright.eu/cy/request/type">My Data Done Right</a>.
+                You can send a email based request via <a className="App-link" target="_blank" href="https://www.mydatadoneright.eu/cy/request/type">My Data Done Right</a>.
                 </p>
                 <p>
                 DARA is a community effort, if you think this page supports browser based data access requests, please consider contributing a connector
-                at <a className="App-link" href="https://github.com/">Github</a>.
+                at <a className="App-link" target="_blank" href="https://github.com/ProgPrak21/react-ts-extension">Github</a>.
                 </p>
             </Grid>
         </>
