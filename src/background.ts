8287dddf922fb3a4812526b0aa147c3ecd800c8d
flow <git@lowspace.de>
2021-07-05 21:19:59 +0200

    Add License and refactor background

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,11 @@
 import * as Con from "./connectors/.";
-import * as Utils from './pageUtils';
+import * as Utils from "./pageUtils";
 
 chrome.runtime.onInstalled.addListener(async () => {
   let connectors = Object.values(Con);
   const url = "https://raw.githubusercontent.com/justgetmydata/jgmd/master/_data/sites.json"
 
-  let response = await fetch(url).then(response => response.json())
+  let response = await fetch(url).then(responseRaw => responseRaw.json())
 
   response.forEach((element: any) => {
     element.description = element.notes_en;
@@ -19,48 +19,57 @@ chrome.runtime.onInstalled.addListener(async () => {
   });
 });
 
+const handleAction = async (message: any) => {
+  let tab: any = await Utils.getCurrentTab();
+  const hostname = message.hostname ?? (new URL(tab.url ?? "")).hostname;
+  const connector: any = await Utils.getConnector(hostname);
+  if (connector && tab) {
+    const { action } = message;
+    if (connector.requestUrl) {
+      tab = await Utils.loadUrl(tab, connector.requestUrl, message.create)
+    }
+    if (tab === false) {
+      chrome.runtime.sendMessage({
+        actionResponse:
+          "Didn't reach the company's request page. Probably, you need to log in first.",
+      });
+    } else {
+      console.log(`Send Message to content script to execute ${action}`);
+      chrome.tabs.sendMessage(<number>tab.id, { action: action });
+    }
+  } else {
+    console.log(`Could not find connector matching ${tab.url}.`);
+  }
+}
+
+const handleGetConnector = async () => {
+  const tab: any = await Utils.getCurrentTab();
+  const { hostname } = new URL(tab.url ?? "");
+  const connector = await Utils.getConnector(hostname);
+  if (connector && tab) {
+    console.log("Sending response", { connector: connector });
+    chrome.runtime.sendMessage({ connector: connector });
+  } else {
+    console.log(`Could not find connector matching ${tab.url}.`);
+    chrome.runtime.sendMessage({ notSupported: true });
+  }
+}
+
+const handleDownload = async (message: any) => {
+  chrome.downloads.download({
+    url: message.downloadUrl,
+    filename: message.downloadName,
+  });
+  chrome.runtime.sendMessage({ actionResponse: "Your Download is ready." });
+}
+
 chrome.runtime.onMessage.addListener(async (message) => {
   console.log("Message received in background!", message);
-
   if (message.action) {
-    let tab: any = await Utils.getCurrentTab();
-    const hostname = message.hostname ?? (new URL(tab.url ?? "")).hostname;
-    const connector: any = await Utils.getConnector(hostname);
-    if (connector && tab) {
-      const { action } = message;
-      if (connector.requestUrl) {
-        tab = await Utils.loadUrl(tab, connector.requestUrl, message.create)
-      }
-      if (tab === false) {
-        chrome.runtime.sendMessage({
-          actionResponse:
-            "Didn't reach the company's request page. Probably, you need to log in first.",
-        });
-      } else {
-        console.log(`Send Message to content script to execute ${action}`);
-        chrome.tabs.sendMessage(<number>tab.id, { action: action });
-      }
-    } else {
-      console.log(`Could not find connector matching ${tab.url}.`);
-    }
-
+    handleAction(message);
   } else if (message.getConnector) {
-    const tab: any = await Utils.getCurrentTab();
-    const { hostname } = new URL(tab.url ?? "");
-    const connector = await Utils.getConnector(hostname);
-    if (connector && tab) {
-      console.log("Sending response", { connector: connector });
-      chrome.runtime.sendMessage({ connector: connector });
-    } else {
-      console.log(`Could not find connector matching ${tab.url}.`);
-      chrome.runtime.sendMessage({ notSupported: true });
-    }
-
+    handleGetConnector();
   } else if (message.downloadUrl) {
-    chrome.downloads.download({
-      url: message.downloadUrl,
-      filename: message.downloadName,
-    });
-    chrome.runtime.sendMessage({ actionResponse: "Your Download is ready." });
+    handleDownload(message);
   }
 });
